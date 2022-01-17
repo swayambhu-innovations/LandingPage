a16f9a88fb40e3299c85d757e724ebee197e1277
Kumar Saptam <saptampro2003@gmail.com>
2022-01-17 12:46:53 +0530

    Added firebase setup.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'swayambhutestinterview',
+    appId: '1:29914453961:web:eca5f066b3fadbcc3aba18',
+    storageBucket: 'swayambhutestinterview.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAtjccxbCfjXNkgkE5IQTvT-y1CwkhsUJM',
+    authDomain: 'swayambhutestinterview.firebaseapp.com',
+    messagingSenderId: '29914453961',
+    measurementId: 'G-2M0D016BLT',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'swayambhutestinterview',
+    appId: '1:29914453961:web:eca5f066b3fadbcc3aba18',
+    storageBucket: 'swayambhutestinterview.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAtjccxbCfjXNkgkE5IQTvT-y1CwkhsUJM',
+    authDomain: 'swayambhutestinterview.firebaseapp.com',
+    messagingSenderId: '29914453961',
+    measurementId: 'G-2M0D016BLT',
+  },
   production: false
 };
 
